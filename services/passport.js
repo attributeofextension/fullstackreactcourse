698e1133340f42a166ef5614d6c6e275b844b98c
attributeofextension <leah.carr00@gmail.com>
2018-02-02 15:58:28 +1000

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require("../config/keys");
 
 User = mongoose.model('users');
@@ -13,13 +13,13 @@ passport.deserializeUser((id,done) => {
     .then(user =>{
        done(null,user)
     });
-
 });
 
 passport.use( new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
 },(accessToken, refreshToken, profile, done) => {
   User.findOne({googleId:profile.id})
     .then((existingUser)=> {
